8be91be84a21f032f422cf48409362b9f3a944d6
Mark Winton <winton.mark@icloud.com>
2019-02-20 20:58:41 -0600

    add link to github

--- src/footer.js
+++ src/footer.js
@@ -10,6 +10,7 @@ export default class extends React.Component {
         <ul>
           <li><Link to="/contact">Contact</Link></li>
           <li><Link to="/privacy">Privacy</Link></li>
+          <li><a href="https://github.com/markwinton/activity-card">GitHub</a></li>
         </ul>
       </footer>
     )
