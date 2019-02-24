4da1435c67ba4fe7b13300b355cddc00d4fa7c91
Mark Winton <winton.mark@icloud.com>
2019-02-24 15:53:14 -0600

    remove dead code

--- src/app.jsx
+++ src/app.jsx
@@ -20,13 +20,11 @@ const tokenGuarded = Component => () => (localStorage.getItem('token') ? <Compon
 export default () => (
   <HashRouter>
     <React.Fragment>
-      <React.Suspense fallback={<div />}>
-        <Route exact path="/" component={tokenGuarded(withHeader(Create))} />
-        <Route path="/connect" component={Connect} />
-        <Route path="/contact" component={withHeader(Contact)} />
-        <Route path="/privacy" component={withHeader(Privacy)} />
-        <Route path="/error" component={withHeader(Error)} />
-      </React.Suspense>
+      <Route exact path="/" component={tokenGuarded(withHeader(Create))} />
+      <Route path="/connect" component={Connect} />
+      <Route path="/contact" component={withHeader(Contact)} />
+      <Route path="/privacy" component={withHeader(Privacy)} />
+      <Route path="/error" component={withHeader(Error)} />
       <Footer />
     </React.Fragment>
   </HashRouter>
