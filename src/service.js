2700811ddabe4745e190283e3e439b598c040c03
Mark Winton <winton.mark@icloud.com>
2019-03-01 16:09:33 -0600

    cleanup promises

--- src/service.js
+++ src/service.js
@@ -8,22 +8,19 @@ export class ServiceError extends Error {
 }
 
 function request(method, url, headers) {
-  return new Promise((resolve, reject) => {
-    fetch(url, {
-      method,
-      headers,
-    })
-      .then((response) => {
-        if (response.status === 200) {
-          resolve(response.json());
-        } else if (response.status === 401) {
-          reject(ServiceError('unauthorized'));
-        } else {
-          reject(ServiceError('unavailable'));
-        }
-      })
-      .catch(() => reject(ServiceError('unavailable')));
-  });
+  return fetch(url, {
+    method,
+    headers,
+  })
+    .then((response) => {
+      if (response.status === 200) {
+        return response.json();
+      }
+      if (response.status === 401) {
+        throw ServiceError('unauthorized');
+      }
+      throw ServiceError('unavailable');
+    });
 }
 
 export function authorize(authorizationCode) {
