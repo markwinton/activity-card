445988e08c3cc24b46176dbc8d491cd428489ba0
Mark Winton <winton.mark@icloud.com>
2019-04-19 19:19:04 -0500

    Revert "remove extended contact info"
    This reverts commit 6f0acd59c0db139433e149046bda4be83b5003a3.

--- src/contact.jsx
+++ src/contact.jsx
@@ -7,5 +7,12 @@ export default () => (
     <h1>Contact</h1>
     <p className="name">Mark Winton</p>
     <p className="email"><a href="mailto:winton.mark@icloud.com?subject=Activity%20Card">winton.mark@icloud.com</a></p>
+    <p>
+      Feel free to reach out on
+      <a href="https://www.linkedin.com/in/markwinton"> LinkedIn</a>
+      , or follow me on
+      <a href="https://www.strava.com/athletes/markwinton"> Strava</a>
+      .
+    </p>
   </section>
 );
