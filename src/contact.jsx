b01b65b4438240d80fda2837db8d3d93f66dc09c
Mark Winton <winton.mark@icloud.com>
2019-04-20 09:52:53 -0500

    remove parenthesis

--- src/contact.jsx
+++ src/contact.jsx
@@ -7,6 +7,6 @@ export default () => (
     <h1>Contact</h1>
     <p className="name">Mark Winton</p>
     <p className="email"><a href="mailto:winton.mark@icloud.com?subject=Activity%20Card">winton.mark@icloud.com</a></p>
-    <p><a href="https://www.strava.com/athletes/markwinton">(Strava)</a></p>
+    <p><a href="https://www.strava.com/athletes/markwinton">Strava</a></p>
   </section>
 );
