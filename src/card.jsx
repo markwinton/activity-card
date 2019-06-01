dea91d92be9bef38b3f80077fa64139881586153
Mark Winton <mark@homerlearning.com>
2019-06-01 10:53:10 -0500

    fix event listener

--- src/card.jsx
+++ src/card.jsx
@@ -66,7 +66,7 @@ export default class extends React.Component {
   }
 
   componentWillUnmount() {
-    window.removeEventListener(this.resizeListener);
+    window.removeEventListener('resize', this.resizeListener);
   }
 
   layout() {
